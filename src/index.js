8a83b08eb052b71e4bf4cdd6e68d8d440b505f62
Golden <golden.butler8@t-mobile.com>
2024-01-01 15:57:22 -0600

    adding vercel anyalytics...

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import './index.css';
 import { CryptoUI } from './CryptoUI';
 import reportWebVitals from './reportWebVitals';
 import { sendToVercelAnalytics } from './vitals';
+import { Analytics } from '@vercel/analytics/react';
 
 ReactDOM.render(
   <React.StrictMode>
     <CryptoUI />
+    <Analytics />
   </React.StrictMode>,
   document.getElementById('root')
 );
